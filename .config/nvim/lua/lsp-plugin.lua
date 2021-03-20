815d1ab222b2b9982d5aed86d4d34b38a78250c2
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2021-03-20 12:54:54 +0100

    LSP highlight document from cursor position. Both normal and insert mode.

--- .config/nvim/lua/lsp-plugin.lua
+++ .config/nvim/lua/lsp-plugin.lua
@@ -18,6 +18,21 @@ local on_attach = function(client, bufnr)
     buf_set_keymap("n", "==", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
+  -- Highlight for current text position
+  -- todo: slow!
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec([[
+      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+  end
 end
 
 
