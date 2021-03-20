947112bcdd907be22638c8fe814a52991eb38a1c
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2021-03-20 18:50:44 +0100

    Made battery show on condition battery exist 💣

--- .config/qtile/bars.py
+++ .config/qtile/bars.py
@@ -4,6 +4,10 @@ from libqtile import bar, widget
 import subprocess
 hasBattery = get_batterystatus = subprocess.check_output(['acpi', '-i']).decode('utf-8').strip() != ""
 
+# TODO: find cleaner way
+batteryText = widget.TextBox("| Battery: ", foreground="#d75f5f") if hasBattery else null
+batteryWidget = widget.Battery() if hasBattery else null
+
 main_bar = bar.Bar([
         widget.CurrentLayout(),
         widget.GroupBox(),
@@ -17,9 +21,9 @@ main_bar = bar.Bar([
             ),
         widget.TextBox("| Volume: ", foreground="#d75f5f"),
         widget.Volume(),
+        batteryText,
+        batteryWidget,
         widget.TextBox("|", foreground="#d75f5f"),
-        #hasBattery ? widget.TextBox("| Battery: ", foreground="#d75f5f"), : null,
-        #hasBattery ? widget.Battery(), : null,
         widget.Systray(),
         widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
         #widget.QuickExit(),
