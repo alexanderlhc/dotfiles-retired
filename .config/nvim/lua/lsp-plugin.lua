48e6a8ff1d529ca54212d81c8de91d6577acaee5
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2021-03-20 12:52:40 +0100

    LSP keymappings: - `gi`      : goto implementation - `<space>d`: show diagnostics - `==`      : format file/range

--- .config/nvim/lua/lsp-plugin.lua
+++ .config/nvim/lua/lsp-plugin.lua
@@ -6,9 +6,17 @@ local on_attach = function(client, bufnr)
 
   -- mappings (should be moved)
   local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<space>d', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+  if client.resolved_capabilities.document_formatting then
+    buf_set_keymap("n", "==", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  elseif client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("n", "==", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
 
 end
 
