9cf64e0e22319fa00b21628d1ef986830f0207b3
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2021-05-02 21:45:14 +0200

    Default to have all maintained supported languages.

--- .config/nvim/lua/plugin-treesitter.lua
+++ .config/nvim/lua/plugin-treesitter.lua
@@ -2,6 +2,7 @@ vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
 
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained", -- all maintained parsers
   highlight = {
     enable = true,
   },
