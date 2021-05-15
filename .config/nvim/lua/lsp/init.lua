16ce0c0cfc40b5236cbdbe4e92c14e49c55aa7af
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2021-05-15 21:03:51 +0200

    Minimize bloat around lsp diagnostics

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,3 +1,14 @@
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = false,
+    --virtual_text = {
+    --  prefix = "",
+    --  spacing = 0,
+    --},
+    signs = true,
+    underline = true,
+  }
+)
 vim.lsp.protocol.CompletionItemKind = {
     "   (Text) ",
     "   (Method)",
