f8eea451b3994f11511da3859850e70cf6693dcf
Alexander Lønbæk Hahn Christensen <alexanderlhc@disroot.org>
2021-05-26 21:54:27 +0200

    Open log in new window

--- .config/nvim/lua/lsp/flutter-tools.lua
+++ .config/nvim/lua/lsp/flutter-tools.lua
@@ -1,3 +1,7 @@
 require("flutter-tools").setup{
-  flutter_path = "/usr/bin/flutter"
+  flutter_path = "/usr/bin/flutter",
+
+  dev_log = {
+    open_cmd = "tabedit", -- command to use to open the log buffer
+  },
 }
